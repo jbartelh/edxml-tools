e916c5b6fd238ca148ed38b5c98dfe64ba71f6e4
jbartelh <joel.bartelheimer@hs-osnabrueck.de>
2016-06-10 16:26:40 +0200

    Packaging for pip

--- setup.py
+++ setup.py
@@ -1,5 +1,15 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(name='XmlStudioCleaner',
       version='1.0.2',
-      py_modules=['clean_studio_xml'])
+      packages=find_packages(),
+      install_requires=[
+          'PyRSS2Gen',
+          'gdata',
+          'youtube-dl'],
+      entry_points={
+          'console_scripts': [
+              'xmlstudioclean = src.clean_studio_xml:main',
+          ],
+      })
+
